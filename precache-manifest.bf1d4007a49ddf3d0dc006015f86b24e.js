08bbd3fd286e1e830438b3f69140527f13ca2823
Josce <nanayhachi94@gmail.com>
2018-11-13 17:28:33 -0600

    Updates

---
+++ precache-manifest.bf1d4007a49ddf3d0dc006015f86b24e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0d4ba95f4ee9b1f6d63",
+    "url": "/babynames/static/css/main.3ec2f999.chunk.css"
+  },
+  {
+    "revision": "c0d4ba95f4ee9b1f6d63",
+    "url": "/babynames/static/js/main.c0d4ba95.chunk.js"
+  },
+  {
+    "revision": "21852b2e2730aa8998ea",
+    "url": "/babynames/static/js/1.21852b2e.chunk.js"
+  },
+  {
+    "revision": "ba496129ff20d267fd5e",
+    "url": "/babynames/static/js/runtime~main.ba496129.js"
+  },
+  {
+    "revision": "fd564aeafb31849df87442ff3281d4a2",
+    "url": "/babynames/index.html"
+  }
+];
